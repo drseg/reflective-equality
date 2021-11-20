bace4eb06ca6fd713479668fcaccc8ea34098a23
Daniel Segall <segalld@gmail.com>
2021-11-20 17:13:55 +0000

    Refactor to private methods

--- Sources/EquatableByReflection/EquatableByReflection.swift
+++ Sources/EquatableByReflection/EquatableByReflection.swift
@@ -3,66 +3,66 @@ import Foundation
 struct Equaliser {
     
     func isEqual(_ lhs: Any, _ rhs: Any) -> Bool {
-        var typesDiffer: Bool {
-            type(of: lhs) != type(of: rhs)
-        }
-        
-        var isEnumWithoutAssociatedValues: Bool {
-            let mirror = Mirror(reflecting: lhs)
-            return mirror.displayStyle == .enum && mirror.children.isEmpty
-        }
-        
-        func properties(of obj: Any) -> [String] {
-            func childProperties(of o: Any) -> Any? {
-                let mirror = Mirror(reflecting: o)
-                
-                var isClass: Bool {
-                    mirror.displayStyle == .class
-                }
-                
-                var hasChildren: Bool {
-                    mirror.children.count != 0
-                }
-                
-                var comparableClassDescription: Any {
-                    func isDescribedByValue(_ description: String) -> Bool {
-                        description.first != "<"
-                    }
-                    
-                    func getClassName(_ description: String) -> Any {
-                        description.split(separator: ":").first!.dropFirst()
-                    }
-                    
-                    let description = String(describing: o)
-                    
-                    return isDescribedByValue(description)
-                    ? description
-                    : getClassName(description)
-                }
-                
-                return hasChildren
-                ? properties(of: o)
-                : (isClass ? comparableClassDescription : o)
-            }
-            
-            let properties = Mirror(reflecting: obj)
-                .children
-                .map(\.value)
-                .compactMap(childProperties)
-            
-            return properties.isEmpty
-            ? [String(describing: obj)]
-            : [String(describing: properties)]
-        }
-        
-        if typesDiffer {
+        if typesDiffer(lhs, rhs) {
             return false
         }
         
-        if isEnumWithoutAssociatedValues {
+        if isEnumWithoutAssociatedValues(lhs) {
             return String(describing: lhs) == String(describing: rhs)
         }
         
         return properties(of: lhs) == properties(of: rhs)
+    }
+    
+    private func typesDiffer(_ lhs: Any, _ rhs: Any) -> Bool {
+        type(of: lhs) != type(of: rhs)
+    }
+    
+    private func isEnumWithoutAssociatedValues(_ candidate: Any) -> Bool {
+        let mirror = Mirror(reflecting: candidate)
+        return mirror.displayStyle == .enum && mirror.children.isEmpty
+    }
+    
+    private func properties(of obj: Any) -> [String] {
+        let properties = Mirror(reflecting: obj)
+            .children
+            .map(\.value)
+            .compactMap(childProperties)
+        
+        return properties.isEmpty
+        ? [String(describing: obj)]
+        : [String(describing: properties)]
+    }
+    
+    private func childProperties(of obj: Any) -> Any? {
+        let mirror = Mirror(reflecting: obj)
+        
+        var isClass: Bool {
+            mirror.displayStyle == .class
+        }
+        
+        var hasChildren: Bool {
+            mirror.children.count != 0
+        }
+        
+        return hasChildren
+        ? properties(of: obj)
+        : (isClass ? comparableClassDescription(of: obj) : obj)
+    }
+    
+    private func comparableClassDescription(of obj: Any) -> Any {
+        func isDescribedByValue(_ description: String) -> Bool {
+            description.first != "<"
+        }
+        
+        func getClassName(_ description: String) -> Any {
+            description.split(separator: ":").first!.dropFirst()
+        }
+        
+        let description = String(describing: obj)
+        
+        return isDescribedByValue(description)
+        ? description
+        : getClassName(description)
     }
 }
