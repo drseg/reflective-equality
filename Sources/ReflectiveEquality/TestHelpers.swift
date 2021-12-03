32ebc6e79de5b664d5de0de4b990be6407b62325
Daniel Segall <segalld@gmail.com>
2021-12-03 15:25:14 +0000

    Update line and file passing in test helper

--- Sources/ReflectiveEquality/TestHelpers.swift
+++ Sources/ReflectiveEquality/TestHelpers.swift
@@ -1,20 +1,20 @@
 import Foundation
 import XCTest
 
-public func assertSameValue(_ lhs: Any, _ rhs: Any) {
-    assertSameValue([lhs, rhs])
+public func assertSameValue(_ lhs: Any, _ rhs: Any, file: StaticString = #file, line: UInt = #line) {
+    assertSameValue([lhs, rhs], file: file, line: line)
 }
 
-public func assertNotSameValue(_ lhs: Any, _ rhs: Any) {
-    assertNotSameValue([lhs, rhs])
+public func assertNotSameValue(_ lhs: Any, _ rhs: Any, file: StaticString = #file, line: UInt = #line) {
+    assertNotSameValue([lhs, rhs], file: file, line: line)
 }
 
-public func assertSameValue(_ args: [Any]) {
-    assert(XCTAssertTrue, args)
+public func assertSameValue(_ args: [Any], file: StaticString = #file, line: UInt = #line) {
+    assert(XCTAssertTrue, args, file: file, line: line)
 }
 
-public func assertNotSameValue(_ args: [Any]) {
-    assert(XCTAssertFalse, args)
+public func assertNotSameValue(_ args: [Any], file: StaticString = #file, line: UInt = #line) {
+    assert(XCTAssertFalse, args, file: file, line: line)
 }
 
 fileprivate func assert(_ assertion: (@autoclosure () throws -> Bool, @autoclosure () -> String, StaticString, UInt) -> (), _ args: [Any], file: StaticString = #file, line: UInt = #line) {
