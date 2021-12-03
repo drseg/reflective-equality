0509372dbd0a5e03c1144bb7c5254bb4b5ad60e2
Daniel Segall <segalld@gmail.com>
2021-12-03 17:46:10 +0000

    Cleanup

--- Sources/ReflectiveEquality/TestHelpers.swift
+++ Sources/ReflectiveEquality/TestHelpers.swift
@@ -22,17 +22,26 @@ fileprivate func assert(_ assertion: (@autoclosure () throws -> Bool, @autoclosu
 }
 
 func generateEqualErrorMessage(_ args: [Any]) -> String {
-    switch args.count {
-    case 0:
-        return ""
-    case 1:
-        return "\(String(describing:args.first!)) must equal itself"
-    case 2:
-        return "\nActual: \(String(describing: args[0]))\nExpected: \(String(describing: args[1]))"
-    default:
-        return args.enumerated().reduce("") {
+    var oneArgErrorMessage: String {
+        "\(String(describing:args.first!)) must equal itself"
+    }
+    
+    var twoArgsErrorMessage: String {
+        "\nActual: \(String(describing: args[0]))" +
+        "\nExpected: \(String(describing: args[1]))"
+    }
+    
+    var multiArgsErrorMessage: String {
+        args.enumerated().reduce("") {
             $0 + "\nArg \($1.offset + 1): \(String(describing: $1.element))"
         }
+    }
+    
+    switch args.count {
+    case 1: return oneArgErrorMessage
+    case 2: return twoArgsErrorMessage
+    case 3...: return multiArgsErrorMessage
+    default: return ""
     }
 }
 
--- Tests/ReflectiveEqualityTests/ReflectiveEqualityTests.swift
+++ Tests/ReflectiveEqualityTests/ReflectiveEqualityTests.swift
@@ -384,19 +384,23 @@ class MultiArgTests: ReflectiveEqualityTests {
 class ErrorHandlingTests: XCTestCase {
     
     func testEqualErrorConditions() {
-        XCTAssertEqual(generateEqualErrorMessage([]), "")
-        XCTAssertEqual(generateEqualErrorMessage([1]), "1 must equal itself")
-        XCTAssertEqual(generateEqualErrorMessage([1, 1]), "\nActual: 1\nExpected: 1")
-        XCTAssertEqual(generateEqualErrorMessage([1, 1, 1]), "\nArg 1: 1\nArg 2: 1\nArg 3: 1")
+        let error = generateEqualErrorMessage
+        
+        XCTAssertEqual(error([]), "")
+        XCTAssertEqual(error([1]), "1 must equal itself")
+        XCTAssertEqual(error([1, 1]), "\nActual: 1\nExpected: 1")
+        XCTAssertEqual(error([1, 1, 1]), "\nArg 1: 1\nArg 2: 1\nArg 3: 1")
+        XCTAssertEqual(error([1, 1, 1, 1]), "\nArg 1: 1\nArg 2: 1\nArg 3: 1\nArg 4: 1")
     }
     
     func testNonEqualErrorConditions() {
         let prefix = "All arguments were unexpectedly equal to "
+        let error = generateNonEqualErrorMessage
         
-        XCTAssertEqual(generateNonEqualErrorMessage([]), prefix + "empty")
-        XCTAssertEqual(generateNonEqualErrorMessage([1]), prefix + "1")
-        XCTAssertEqual(generateNonEqualErrorMessage([1, 1]), prefix + "1")
-        XCTAssertEqual(generateNonEqualErrorMessage([1, 1, 1]), prefix + "1")
+        XCTAssertEqual(error([]), prefix + "empty")
+        XCTAssertEqual(error([1]), prefix + "1")
+        XCTAssertEqual(error([1, 1]), prefix + "1")
+        XCTAssertEqual(error([1, 1, 1]), prefix + "1")
     }
 }
 
