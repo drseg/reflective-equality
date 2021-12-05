bfc4d523521c02a70fc793c6c19fc192087cb655
Daniel Segall <segalld@gmail.com>
2021-12-05 14:24:23 +0000

    Add new underlying tests

--- Tests/ReflectiveEqualityTests/StringMirrorTests.swift
+++ Tests/ReflectiveEqualityTests/StringMirrorTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class StringDescribingTests: ReflectiveEqualityTests {
 
-    func test_instancesCanBeDescribed() {
+    func test_someInstancesCanBeDescribedAsExpected() {
         1  => "1"
         1.1 => "1.1"
         
@@ -17,7 +17,11 @@ final class StringDescribingTests: ReflectiveEqualityTests {
         [1: 1] => "[1: 1]"
     }
     
-    func test_someClassesAreDescribedByNameAndID() {
+    func test_descriptionsAreComparable() {
+        1 => String(describing: 1)
+    }
+    
+    func test_someClassesAreDescribedByClassNameAndID() {
         let object = String(describing: nsO)
         
         object.first! => "<"
@@ -28,11 +32,24 @@ final class StringDescribingTests: ReflectiveEqualityTests {
         object.dropFirst(11).dropLast().count => "11"
     }
     
-    func test_otherClassesAreDescribedByValue() {
-        "Cat".ns => "Cat"
+    func test_classesDescribedByNameAreNotFullyComparable() {
+        nsO !=> String(describing: nsO) // ideally should be equal by value
     }
     
-    func test_someClassValuesAreUnexpected() {
+    func test_classesDescribedByNameHaveComparableClassNames() {
+        String(describing: nsO).prefix(9).dropFirst() => String(describing: nsO).prefix(9).dropFirst()
+        String(describing: nsO).prefix(9).dropFirst() => "NSObject"
+    }
+    
+    func test_otherClassesAreDescribedByValue_AndAreComparable() {
+        "Cat".ns => "Cat"
+        CGFloat(1.1) => "1.1"
+    
+        "Cat".ns => String(describing: "Cat".ns)
+        "Cat".ns !=> String(describing: "Bat".ns)
+    }
+    
+    func test_someClassValuesAreUnexpected_YetRemainComparable() {
         [1, 2].ns => """
                     (
                         1,
@@ -44,13 +61,83 @@ final class StringDescribingTests: ReflectiveEqualityTests {
                         1 = 2;
                     }
                     """
+    
+        [1, 2].ns => String(describing: [1, 2].ns)
+        [1, 2].ns !=> String(describing: [1, 3].ns)
+        
+        [1: 2].ns => String(describing: [1: 2].ns)
+        [1: 2].ns !=> String(describing: [1: 3].ns)
+    }
+    
+    func test_structsAreDescribedByNameAndValues_AndAreComparable() {
+        struct Struct {
+            let a = 1, b = "b", c = [1, 2, 3, 4, 5]
+        }
+        
+        Struct() => "Struct(a: 1, b: \"b\", c: [1, 2, 3, 4, 5])"
+        Struct() => String(describing: Struct())
+    }
+    
+    func test_customClassesAreOpaquelyDescribed_AndNotComparable() {
+        class CustomClass {
+            let a: Int, b: String, c: [Int]
+            
+            init(_ a: Int, _ b: String, _ c: [Int]) {
+                self.a = a
+                self.b = b
+                self.c = c
+            }
+        }
+        
+        let c1 = CustomClass(1, "b", [1, 2, 3, 4, 5])
+        let c2 = CustomClass(2, "c", [2, 3, 4, 5, 6])
+        
+        c1 => String(describing: c2) // should not be equal
+    }
+    
+    func test_customClassesDescribeTheirNamesAfterFinalDot() {
+        class First {}; class Second {}
+        
+        func className(_ instance: Any) -> String {
+            String(String(describing: instance).split(separator: ".").last!)
+        }
+        
+        XCTAssertEqual(className(First()), className(First()))
+        XCTAssertNotEqual(className(First()), className(Second()))
+    }
+    
+    func test_closuresAreNotDescribedOrComparable() {
+        let closure1: (Bool) -> (Bool) = { $0 }
+        let closure2: (Bool, Bool) -> (Bool) = { $0 && $1 }
+        
+        closure1 => "(Function)"
+        closure1 => String(describing: closure2)
+    }
+    
+    func test_enumsAreDescribedByCaseName() {
+        enum Enum { case a, b }
+        
+        Enum.a => "a"
+        Enum.b => "b"
+    }
+    
+    func test_enumsWithAssociatedValuesAreDescribedByCaseNameAndValue() {
+        enum Enum { case a(Int), b(Int) }
+        
+        Enum.a(1) => "a(1)"
+        Enum.b(2) => "b(2)"
     }
 }
 
 final class MirrorTests: XCTestCase { }
 
 infix operator =>
+infix operator !=>
 
-func =>(_ actual: Any, _ expected: String) {
-    XCTAssertEqual(String(describing: actual), expected)
+func =><S: StringProtocol>(_ actual: Any, _ expected: S) {
+    XCTAssertEqual(String(describing: actual), String(expected))
+}
+
+func !=><S: StringProtocol>(_ actual: Any, _ expected: S) {
+    XCTAssertNotEqual(String(describing: actual), String(expected))
 }
